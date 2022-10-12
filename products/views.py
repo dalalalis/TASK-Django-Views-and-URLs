49320310689c7628d549a05708a387a96eadb2c3
Dalal Alsabah <dasabah@gmail.com>
2022-10-12 19:16:21 +0300

    solved the task from first attempt

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+def get_product(request, product_id):
+    product=Product.objects.get (id= product_id)
+    return HttpResponse(f"The product ordered is a <h1>{product}</h1> ")
+def get_home(request):
+    return HttpResponse ("Welcome to Online Store")
