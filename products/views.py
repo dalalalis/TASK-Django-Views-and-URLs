fbb8557781d925a27a6edeffce1c8d27488591ba
Dalal Alsabah <dasabah@gmail.com>
2022-10-17 13:55:56 +0300

    list page done

--- products/views.py
+++ products/views.py
@@ -4,10 +4,19 @@ from .models import Product
 # Create your views here.
 def get_product(request, product_id):
     product=Product.objects.get (id= product_id)
-    return HttpResponse(f""" <p>{product.id} </p>
-     <p1>{product.name}</p1> 
-     <p1>{product.price}</p>
-     """)
+    context={
+        "product":{
+            "id":product_id,
+            "name":product.name,
+            "description":product.description,
+            "price":product.price }
+        }
+    
+    return render (request, "product_detail.html", context)
+    #return HttpResponse(f""" <p>{product.id} </p>
+     #<p1>{product.name}</p1> 
+     #<p1>{product.price}</p>
+     #""")
      #three quatations for multiple lines 
     
 def get_home(request):
