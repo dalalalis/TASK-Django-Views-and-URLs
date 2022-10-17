269c0bbe11656313baf845677bc7c1cb2f5e0552
Dalal Alsabah <dasabah@gmail.com>
2022-10-17 15:30:24 +0300

    list page done

--- products/views.py
+++ products/views.py
@@ -19,5 +19,15 @@ def get_product(request, product_id):
      #""")
      #three quatations for multiple lines 
     
+def get_products(request):
+    products=Product.objects.all()
+    list_products=[]
+    for product in products:
+        list_products.append({"name":product.name,  "price":product.price})
+
+    context={"products":list_products }
+    return render (request, "product-list.html",context)
+
+
 def get_home(request):
     return HttpResponse ("Welcome to Online Store")
--- productshop/urls.py
+++ productshop/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from products.views import get_home, get_product
-from products.views import get_product
+from products.views import get_home
+from products.views import get_product, get_products
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("home/", get_home, name= "home"),
-    path ("products/<int:product_id>/", get_product, name="product_detail")
-]
+    path("products/", get_products, name= "product_list"),
+    path ("products/<int:product_id>/", get_product, name="product_detail"),
+]   
